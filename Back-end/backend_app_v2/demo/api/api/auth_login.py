74b7bc66550e33b2538ed1a89b94975ce6ea1b1a
NashZhao <nashzhaosyd@gmail.com>
2020-10-25 19:12:39 +1100

    update signup login

--- Back-end/backend_app_v2/demo/api/api/auth_login.py
+++ Back-end/backend_app_v2/demo/api/api/auth_login.py
@@ -14,18 +14,17 @@ class AuthLogin(Resource):
 
     def post(self):
         g.user = request.json
-        user = db.db.user.find_one({"email":request.json["username"]})
+        user = db.db.user.find_one({"email":request.json["username"],"userType":request.json["userType"]})
 
         if not user:
             return "The user not exists", 400, None
 
         if not auth.verigy_hash(g.user["password"],user["password"]):
             return "The password not correct", 400, None
-        access_token = create_access_token(g.user["username"])
-        refresh_token = create_refresh_token(g.user["username"])
+        access_token = create_access_token({"user":g.user["username"],"type":g.user["userType"]})
+        # refresh_token = create_refresh_token(g.user["username"])
 
         return {
                 "message": "Logged in",
-                "access_token": access_token,
-                "refresh_token": refresh_token
+                "access_token": access_token
                  }, 200, None
