6729ce0cea000024d2b6090228b0f0f15744e110
walkenzhong <walkenzhong@gmail.com>
2020-10-25 21:04:24 +1100

    finish image upload

--- Back-end/photopro/api/api/image.py
+++ Back-end/photopro/api/api/image.py
@@ -5,11 +5,14 @@ from flask import request, g
 
 from . import Resource
 from .. import schemas
-
+import os
 
 class Image(Resource):
 
     def post(self):
         print(g.form)
-
-        return None, 200, None
+        f = request.files['file']
+        print(f.filename)
+        print(str(os.path.abspath('.')))
+        f.save(str(os.path.abspath('.')) + '/upload/'+f.filename)
+        return [1], 200, None
