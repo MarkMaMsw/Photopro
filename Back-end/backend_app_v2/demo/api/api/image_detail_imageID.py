db5e9ed9e8705ed5f9d3de7e7d29e849627d48d6
NashZhao <nashzhaosyd@gmail.com>
2020-10-25 18:31:57 +1100

    update swagger file

---
+++ Back-end/backend_app_v2/demo/api/api/image_detail_imageID.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
+from flask import request, g
+
+from . import Resource
+from .. import schemas
+
+
+class ImageDetailImageid(Resource):
+
+    def get(self, imageID):
+
+        return {}, 200, None
+
+    def put(self, imageID):
+        print(g.json)
+
+        return None, 200, None
---
+++ Back-end/backend_app_v2/demo/api/db.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_pymongo import pymongo
+# from app import app
+CONNECTION_STRING = "mongodb+srv://COMP9900_group:comp9900@cluster0.okzv2.mongodb.net/<dbname>?retryWrites=true&w=majority"
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('flask_mongodb_atlas')
+user_collection = pymongo.collection.Collection(db, 'user')
