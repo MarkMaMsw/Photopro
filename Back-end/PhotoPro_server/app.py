f3f346ea7e7fa7ec9c15ad6c487c01f2ee3a1952
walkenzhong <walkenzhong@gmail.com>
2020-10-31 15:44:26 +1100

    add get all image of one contributor

--- Back-end/PhotoPro_server/app.py
+++ Back-end/PhotoPro_server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, g
 from flask_cors import CORS
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
@@ -6,6 +6,7 @@ from flask_jwt_extended import JWTManager
 from login import AuthLogin
 from register import AuthRegister
 from contributorServices.contributor import ContributorInfo
+from contributorServices.contributorImage import ContributorImage
 from imageServices.imageUpload import ImageUpload
 from imageServices.imageDetail import ImageDetail
 from imageServices.imageFile import ImageFile
@@ -20,11 +21,8 @@ jwt = JWTManager(app)
 
 # use http://127.0.0.1:5000/docs/api/ to see API document
 app.config['RESTFUL_API_DOC_EXCLUDE'] = []
-
 restful_api = Api(app)
 
-file_store = 'upload/'
-app.config['UPLOAD_FOLDER'] = file_store
 
 # loginService
 restful_api.add_resource(AuthRegister, '/register')
@@ -33,6 +31,7 @@ restful_api.add_resource(ImageDetail, '/image/<string:imageId>')
 restful_api.add_resource(ImageUpload, '/image')
 restful_api.add_resource(ImageFile, '/image/file/<string:imageName>')
 restful_api.add_resource(ContributorInfo, '/contributor')
+restful_api.add_resource(ContributorImage, '/contributor/<string:contributorId>')
 
 
 if __name__ == '__main__':
---
+++ Back-end/PhotoPro_server/dev/config.py
@@ -0,0 +1,2 @@
+image_file_url = "/image/file/"
+ip_address = "http://127.0.0.1:5000"
