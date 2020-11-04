f71c61137a408c1ecc62be57345cf7db566a77eb
walkenzhong <walkenzhong@gmail.com>
2020-11-04 16:29:13 +1100

    update some image detail

--- Back-end/PhotoPro_server/contributorServices/contributorImage.py
+++ Back-end/PhotoPro_server/contributorServices/contributorImage.py
@@ -3,22 +3,13 @@ import db
 from flask_restful import Resource
 import json
 import dev.config as config
+import imageServices.imageDetail as imageDetail
 class ContributorImage(Resource):
     def get_detail_from_db(self,contributorId):
         image = db.db.image.find({"contributor_id":contributorId})
         result = []
         for i in image:
-            image_url = config.ip_address + config.image_file_url + i['image_name']
-            tem_result = {
-                'image_id': i['image_id'], 
-                'contributor_id': i['contributor_id'], 
-                'title': i['title'], 
-                'price': i['price'], 
-                'status': i['status'], 
-                'tag': i['tag'], 
-                'image_name': i['image_name'],
-                'image_url': image_url
-            }
+            tem_result = imageDetail.get_image_detail_from_db(i['image_id'])
             result.append(tem_result)
         return result
     #get image detail
--- Back-end/PhotoPro_server/imageServices/imageDetail.py
+++ Back-end/PhotoPro_server/imageServices/imageDetail.py
@@ -3,20 +3,29 @@ import db
 from flask_restful import Resource
 import json
 import dev.config as config
+import userServices.userInfo as userInfo
 def get_image_detail_from_db(image_id):
     image = db.db.image.find_one({"image_id":image_id})
+    comment_num = db.db.comment.find({"image_id":image_id}).count()
+    like_num = db.db.like.find({"image_id":image_id}).count()
+    #print(comment_num)
+    #print(like_num)
+    contributor = userInfo.get_user_info(image['contributor_id'])
+    #print(contributor)
     #print(type(imageId))
     #print(type(image))
     image_url = config.ip_address + config.image_file_url + image['image_name']
     result = {
         'image_id': image['image_id'], 
-        'contributor_id': image['contributor_id'], 
+        'contributor_detail': contributor, 
         'title': image['title'], 
         'price': image['price'], 
         'status': image['status'], 
         'tag': image['tag'], 
         'image_name': image['image_name'],
-        'image_url': image_url
+        'image_url': image_url,
+        'like_num' : like_num,
+        'comment_num' : comment_num
     }
     return result
 class ImageDetail(Resource):
