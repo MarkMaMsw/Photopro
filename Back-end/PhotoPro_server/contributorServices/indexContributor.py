b036c1dd27cc8c38383f4295d34e9873368c3e50
NashZhao <nashzhaosyd@gmail.com>
2020-11-09 20:27:49 +1100

    implement top image/contributor

--- Back-end/PhotoPro_server/contributorServices/indexContributor.py
+++ Back-end/PhotoPro_server/contributorServices/indexContributor.py
@@ -6,12 +6,38 @@ import dev.config as config
 import userServices.userInfo as userInfo
 class IndexContributor(Resource):
     def get_detail_from_db(self):
-        image = db.db.user.find()
-        result = []
-        for i in image:
-            tem_result = userInfo.get_user_info(i['id'])
-            result.append(tem_result)
-        return result[-12:]
+        cons = db.db.user.find({"userType":"contributor"})
+        contributors = {}
+        for i in cons:
+            temp = userInfo.get_user_info(i["id"])
+            temp["like_sum"] = 0
+            contributors[i["id"]]=temp
+        like = db.db.like.find({"like_status":"active"})
+        # print(like)
+        for i in like:
+            # print(i)
+            i.pop("_id")
+            image = db.db.image.find_one({"image_id":i["image_id"]})
+            # print(image)
+            if image != None:
+                author = db.db.user.find_one({"id":image["contributor_id"]})
+                # print(author)
+                author = userInfo.get_user_info(author["id"])
+                contributors[author["id"]]["like_sum"]+=1
+                    
+                # print(contributors)
+        conList = []
+        for key,value in contributors.items():
+            conList.append(value)
+        # print(conList)
+        conList = sorted(conList,key = lambda x: x["like_sum"],reverse=True)
+        print(conList)
+        contriList = []
+        for i in conList:
+            contriList.append(userInfo.get_user_info(i["id"]))
+        # print(contriList)
+        return contriList[:12]
+
     #get image detail
     def get(self):
         all_image = self.get_detail_from_db()
