124a11327b3fabfe2e29ee328e68a684288df265
walkenzhong <walkenzhong@gmail.com>
2020-11-10 18:04:41 +1100

    fix index image

--- Back-end/PhotoPro_server/imageServices/indexImage.py
+++ Back-end/PhotoPro_server/imageServices/indexImage.py
@@ -6,6 +6,7 @@ import dev.config as config
 import imageServices.imageDetail as imageDetail
 class IndexImage(Resource):
     def get_detail_from_db(self):
+        '''
         imageList = []
         images = db.db.image.find({'status': 'on_shop'})
         # print(like)
@@ -21,6 +22,14 @@ class IndexImage(Resource):
             i.pop("like_sum")
             imageArray.append(i)
         return imageArray[:12]
+        '''
+        imageList = []
+        images = db.db.image.find({'status': 'on_shop'})
+        for i in images:
+            tem_image = imageDetail.get_image_detail_from_db(i['image_id'])
+            imageList.append(tem_image)
+        imageList = sorted(imageList,key = lambda x: x["like_num"],reverse=True)
+        return imageList[:12]
     #get image detail
     def get(self):
         all_image = self.get_detail_from_db()
